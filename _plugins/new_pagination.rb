08722393ce0ebc4ab223ca5fe9fa586033bd8daf
Daniel Oakley <danneh@danneh.net>
2013-01-24 23:38:09 +1000

    Updates, getting stuff working, custom pagination plugins

---
+++ _plugins/new_pagination.rb
@@ -0,0 +1,171 @@
+module Jekyll
+
+  class Pagination < Generator
+    # This generator is safe from arbitrary code execution.
+    safe true
+
+    # Generate paginated pages if necessary.
+    #
+    # site - The Site.
+    #
+    # Returns nothing.
+    def generate(site)
+      site.pages.dup.each do |page|
+        paginate(site, page) if Pager.pagination_enabled?(site.config, page.name)
+      end
+    end
+
+    # Paginates the blog's posts. Renders the index.html file into paginated
+    # directories, e.g.: page2/index.html, page3/index.html, etc and adds more
+    # site-wide data.
+    #
+    # site - The Site.
+    # page - The index.html Page that requires pagination.
+    #
+    # {"paginator" => { "page" => <Number>,
+    #                   "per_page" => <Number>,
+    #                   "posts" => [<Post>],
+    #                   "total_posts" => <Number>,
+    #                   "total_pages" => <Number>,
+    #                   "previous_page" => <Number>,
+    #                   "next_page" => <Number> }}
+    def paginate(site, page)
+      category = page.data['category']
+
+      if category
+        posts = site.categories[category].reverse
+      else
+        posts = site.site_payload['site']['posts']
+      end
+
+      pages = Pager.calculate_pages(posts, site.config['paginate'].to_i)
+
+      # iterate over the total number of pages and create a physical page for each
+      (1..pages).each do |num_page|
+
+        pager = Pager.new(site.config, num_page, posts, File.join(page.dir, paginate_path(site, category)), pages)
+
+        # the first page is the index, so no page needs to be created. However, the subsequent pages need to be generated
+          if num_page > 1
+            if category
+              newpage = CategorySubPage.new(site, site.source, category, page.data['category_layout'])
+            else
+              newpage = Page.new(site, site.source, page.dir, page.name)
+            end
+
+            newpage.pager = pager
+            newpage.dir = File.join(page.dir, paginate_path(site, category).sub(':num', num_page.to_s))
+            site.pages << newpage
+          else
+            page.pager = pager
+          end
+
+      end
+    end
+
+    private
+      def paginate_path(site, category = false)
+        if category
+          if site.config['paginate_category_path']
+            format = site.config['paginate_category_path']
+          else
+            format = ":category/page:num"
+          end
+          format.sub(':category', category)
+        else
+          site.config['paginate_path']
+        end
+      end
+  end
+
+  class Pager
+    attr_reader :page, :per_page, :posts, :total_posts, :total_pages, :previous_page, :previous_page_url, :next_page, :next_page_url
+
+    # Calculate the number of pages.
+    #
+    # posts - The Array of Posts.
+    # per_page  - The Integer of entries per page.
+    #
+    # Returns the Integer number of pages.
+    def self.calculate_pages(posts, per_page)
+      (posts.size.to_f / per_page.to_i).ceil
+    end
+
+    # Determine if pagination is enabled for a given file.
+    #
+    # config - The configuration Hash.
+    # file   - The String filename of the file.
+    #
+    # Returns true if pagination is enabled, false otherwise.
+    def self.pagination_enabled?(config, file)
+      file == 'index.html' && !config['paginate'].nil?
+    end
+
+    # Initialize a new Pager.
+    #
+    # config    - The Hash configuration of the site.
+    # page      - The Integer page number.
+    # posts     - The Array of Posts.
+    # page_url  - URL to next/prev pages
+    # num_pages - The Integer number of pages or nil if you'd like the number
+    #             of pages calculated.
+    def initialize(config, page, posts, page_url, num_pages = nil)
+      @page = page
+      @per_page = config['paginate'].to_i
+      @total_pages = num_pages || Pager.calculate_pages(posts, @per_page)
+
+      if @page > @total_pages
+        raise RuntimeError, "page number can't be greater than total pages: #{@page} > #{@total_pages}"
+      end
+
+      init = (@page - 1) * @per_page
+      offset = (init + @per_page - 1) >= posts.size ? posts.size : (init + @per_page - 1)
+
+      @total_posts = posts.size
+      @posts = posts[init..offset]
+      @previous_page = @page != 1 ? @page - 1 : nil
+      @previous_page_url = page_url.sub(':num', @previous_page.to_s)
+      @next_page = @page != @total_pages ? @page + 1 : nil
+      @next_page_url = page_url.sub(':num', @next_page.to_s)
+    end
+
+    # Convert this Pager's data to a Hash suitable for use by Liquid.
+    #
+    # Returns the Hash representation of this Pager.
+    def to_liquid
+      {
+        'page' => page,
+        'per_page' => per_page,
+        'posts' => posts,
+        'total_posts' => total_posts,
+        'total_pages' => total_pages,
+        'previous_page' => previous_page,
+        'previous_page_url' => previous_page_url,
+        'next_page' => next_page,
+        'next_page_url' => next_page_url
+      }
+    end
+  end
+
+  # The CategorySubPage class creates a single category page for the specified tag.
+  # This class exists to specify the layout to use for pages after the first index page
+  class CategorySubPage < Page
+    
+    def initialize(site, base, category, layout)
+        
+      @site = site
+      @base = base
+      @dir  = category
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), layout || 'category_index.html')
+
+      title_prefix             = site.config['cateogry_title_prefix'] || ''
+      self.data['title']       = "#{title_prefix}#{category}"
+
+    end
+    
+  end
+
+end
